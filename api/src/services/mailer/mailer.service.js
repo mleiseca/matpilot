45b905d8350f9122b29da6fcabc422a54adbe355
Michael Leiseca <mleiseca@gmail.com>
2019-04-26 09:03:12 -0500

    Adding usermanagement

---
+++ api/src/services/mailer/mailer.service.js
@@ -0,0 +1,18 @@
+// Initializes the `/mailer` service on path `/mailer`
+const hooks = require('./mailer.hooks');
+const Mailer = require('feathers-mailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+
+module.exports = function (app) {
+  app.use('/mailer', Mailer(smtpTransport({
+    host: 'email-smtp.us-east-1.amazonaws.com',
+    secure: true,
+    auth: {
+      user: app.get("smtpUser"), //process.env.SMTP_USER,
+      pass: app.get("smtpPassword") //process.env.SMTP_PASS
+    }
+  })));
+
+  const service = app.service('mailer');
+  service.hooks(hooks);
+};
