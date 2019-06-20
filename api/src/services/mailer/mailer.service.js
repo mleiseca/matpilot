fcfe7fe485bf846ffd39893b69c8922d1816c9ad
Michael Leiseca <mleiseca@gmail.com>
2019-06-19 23:49:14 -0500

    removing plpgsql from schema?

--- api/src/services/mailer/mailer.service.js
+++ api/src/services/mailer/mailer.service.js
@@ -4,12 +4,15 @@ const Mailer = require('feathers-mailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
 module.exports = function (app) {
+  const smtpUser = process.env.SMTP_USER || app.get("smtpUser")
+  const smtpPassword = process.env.SMTP_PASS || app.get("smtpPassword")
+  console.log("SMTP:", smtpUser, smtpUser)
   app.use('/mailer', Mailer(smtpTransport({
     host: 'email-smtp.us-east-1.amazonaws.com',
     secure: true,
     auth: {
-      user: process.env.SMTP_USER || app.get("smtpUser"),
-      pass: process.env.SMTP_PASS || app.get("smtpPassword")
+      user: smtpUser,
+      pass: smtpPassword
     }
   })));
 
