8a9bf9d57b8b9a64754b1c16cb08d3fc9c86570d
Michael Leiseca <mleiseca@gmail.com>
2019-06-19 22:24:29 -0500

    removing plpgsql from schema?

--- api/src/sequelize.js
+++ api/src/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres') || process.env.DB_URL;
+  const connectionString = process.env.DB_URL || app.get('postgres');
   console.log("using connection string: ", connectionString)
   const sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
