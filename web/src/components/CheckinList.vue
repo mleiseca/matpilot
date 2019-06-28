ceb958adaefd4231d05cf384c61c94daa020e92d
Michael Leiseca <mleiseca@gmail.com>
2019-06-27 22:49:07 -0500

    fixing checking list and grouping by day

--- web/src/components/CheckinList.vue
+++ web/src/components/CheckinList.vue
@@ -4,11 +4,12 @@
         <div class="title font-weight-light mb-2">Upcoming events</div>
       </div>
 
-      <v-card-text>
-        <v-flex xs12 v-for="event in events" v-bind:key="event.id">
-          <checkin-event v-bind:event-details="event" />
-        </v-flex>
-      </v-card-text>
+        <div v-for="event in events" v-bind:key="event.date">
+          <v-flex xs12>{{ event.date }} </v-flex>
+          <v-flex xs12 v-for="e in event.events" v-bind:key="e.id">
+            <checkin-event v-bind:event-details="e" />
+          </v-flex>
+        </div>
     </material-card>
 
 </template>
@@ -17,7 +18,7 @@
 import { rrulestr } from 'rrule'
 import momentTz from 'moment-timezone'
 import CheckinEvent from './CheckinEvent.vue'
-// import moment from 'moment'
+import moment from 'moment'
 
 export default {
   components: { CheckinEvent },
@@ -39,26 +40,65 @@ export default {
         const se = ses[seIndex]
         console.log("scheduledEvent", se)
 
-        // TODO: this '5' should really be controlled by a toggle on the material card. maybe day/week/month?
-        const newEvents = rrulestr(se.rrules).all(function (date, i) { return i < 5 })
+//        console.log('rules', se.rrules)
+
+        const newEvents = rrulestr(se.rrules).between(moment().toDate(), moment().add(7, 'days').toDate(), true, function (date, i) {
+          // TODO: this '7' should really be controlled by a toggle on the material card. maybe day/week/month?
+          return i < 7
+        })
 
         for (const newEventIndex in newEvents) {
           const newEvent = newEvents[newEventIndex]
+//          console.log('newEvent', newEvent)
 
           const startDateTime = momentTz.tz(se.startTime, 'HH:mm', se.timezone)
             .year(newEvent.getUTCFullYear()).month(newEvent.getUTCMonth()).date(newEvent.getUTCDate())
           const endDateTime = momentTz.tz(se.endTime, 'HH:mm', se.timezone)
             .year(newEvent.getUTCFullYear()).month(newEvent.getUTCMonth()).date(newEvent.getUTCDate())
+          const startDate = momentTz.tz(newEvent, se.timezone).format('MMMM D')
 
-
-          upcomingEvents.push({ startDateTime: startDateTime, endDateTime: endDateTime, scheduledEvent: se, id: 'se-' + se.id + '-' + startDateTime })
+          upcomingEvents.push({ startDateTime: startDateTime, endDateTime: endDateTime, scheduledEvent: se, id: 'se-' + se.id + '-' + startDateTime, startDate: startDate })
         }
       }
+
+      if (upcomingEvents.length === 0) {
+        this.events = []
+        return;
+      }
+
       upcomingEvents.sort(function (a, b) {
         return a.startDateTime.diff(b.startDateTime)
       })
 
-      this.events = upcomingEvents
+      this.events = []
+
+
+      let currentDayEvents = []
+      let currentDay = upcomingEvents[0].startDate
+
+      for(let i = 0; i< upcomingEvents.length; i++) {
+        let e = upcomingEvents[i]
+
+//        console.log('startDate', e.startDate, currentDay)
+        if (e.startDate !== currentDay) {
+            this.events.push({
+              date: currentDay,
+              events: currentDayEvents
+            })
+          currentDay = e.startDate
+          currentDayEvents = []
+        }
+        currentDayEvents.push(e)
+      }
+
+      this.events.push({
+        date: currentDay,
+        events: currentDayEvents
+      })
+//      console.log('events', this.events)
+
+
+
     })
   }
 }
