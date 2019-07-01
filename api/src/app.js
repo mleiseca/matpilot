1aee3446357e8a705e895a046ea57882b9e25d47
Michael Leiseca <mleiseca@gmail.com>
2019-06-30 23:13:50 -0500

    Helmet the problem?

--- api/src/app.js
+++ api/src/app.js
@@ -26,9 +26,7 @@ const app = express(feathers())
 // Load app configuration
 app.configure(configuration())
 // Enable security, CORS, compression, favicon and body parsing
-// app.use(helmet({
-//   noSniff: false // To allow for HTTP2 support
-// }))
+app.use(helmet())
 app.use(cors())
 app.use(compress())
 app.use(express.json())
