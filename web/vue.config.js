2ea369d06b5074e5a1af4108810be67d27aaae2e
Michael Leiseca <mleiseca@gmail.com>
2019-07-01 02:18:46 -0500

    removing console.log from production breaking up chunks only loading english locale for date formatting

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,15 +1,25 @@
+var webpack = require('webpack')
+
 module.exports = {
   configureWebpack:{
     optimization: {
       splitChunks: {
+        chunks: 'all',
         minSize: 10000,
         maxSize: 512000,
       }
-    }
+    },
+    plugins: [
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en.*/),
+    ]
   },
   pluginOptions: {
     webpackBundleAnalyzer: {
-      openAnalyzer: false
-    }
+      openAnalyzer: false,
+      reportFilename: '.report.html',
+      generateStatsFile: true,
+      statsFilename: '.stats.json'
+    },
+
   }
 }
