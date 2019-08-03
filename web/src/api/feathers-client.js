1ca5f746020c7ca1f4523d0109ea13bddb547676
Michael Leiseca <mleiseca@gmail.com>
2019-08-02 23:53:36 -0500

    Should reconnect if connect is lost...

--- web/src/api/feathers-client.js
+++ web/src/api/feathers-client.js
@@ -5,7 +5,13 @@ import io from 'socket.io-client'
 
 export const host = process.env.VUE_APP_API_HOST || 'https://www.matpilot.com'
 
-const socket = io(host, { transports: ['websocket'] })
+const socket = io(host, {
+  transports: ['websocket'],
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax : 5000,
+  reconnectionAttempts: Infinity
+})
 
 const feathersClient = feathers()
   .configure(socketio(socket, {
