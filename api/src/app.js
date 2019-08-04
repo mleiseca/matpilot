1afcf8450bd0c23084d1413c101a097646e69be7
Michael Leiseca <mleiseca@gmail.com>
2019-08-03 22:53:15 -0500

    maybe this is how to specify max age for static assets?

--- api/src/app.js
+++ api/src/app.js
@@ -38,10 +38,15 @@ app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
 app.use(history({
   // logger: console.log.bind(console)
 }))
-app.use('/', express.static(app.get('public'), {
+app.use('/js', express.static(app.get('public'), {
   immutable: true,
   maxAge: 31557600000 // 1 year
 }))
+app.use('/css', express.static(app.get('public'), {
+  immutable: true,
+  maxAge: 31557600000 // 1 year
+}))
+app.use('/', express.static(app.get('public')))
 
 // Set up Plugins and providers
 app.configure(express.rest())
